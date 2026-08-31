b39be660d806cb56231da4173f969a5a38aeb7c9
Keneriyan <keneriyanfikadu@gmail.com.com>
2026-08-31 18:07:27 +0300

    feat: add CRUD functions for task create/read/update/delete

--- app/database.py
+++ app/database.py
@@ -3,7 +3,6 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./tasks.db"
 
-# check_same_thread=False needed for sqlite + fastapi's threading model
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
